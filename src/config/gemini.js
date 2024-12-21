26a11808996e1b0b49d2902e5eedb7571f70c9f1
NjratTech <86553375+NjratTech@users.noreply.github.com>
2024-12-21 13:58:48 +0300

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -13,30 +13,29 @@ const model = genAI.getGenerativeModel({
 });
 
 const generationConfig = {
-    temperature: 1, 
-    topP: 0.95, 
+    temperature: 1,
+    topP: 0.95,
     topK: 64,
-    maxOutputTokens: 8192, 
+    maxOutputTokens: 8192,
     responseMimeType: "text/plain",
 };
 
-// Настройки безопасности
 const safetySettings = [
     {
         category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
     },
     {
         category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
     },
     {
         category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
     },
     {
         category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
     },
 ];
 
